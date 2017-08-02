2acacb72f2ab7fea112ee375e56a656c9bfa5178
Moana <moana.dumora@viaccess-orca.com>
2017-08-02 17:06:43 +0200

    register + login done (TODO : encryption)

--- app/src/main/java/com/dumbaz/skatespot/LoginActivity.java
+++ app/src/main/java/com/dumbaz/skatespot/LoginActivity.java
@@ -1,12 +1,20 @@
 package com.dumbaz.skatespot;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -34,6 +42,45 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent registerIntent = new Intent(LoginActivity.this, MapsActivity.class);
                 LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject json = new JSONObject(response);
+                            boolean success = json.getBoolean("success");
+                            if (success) {
+                                String name = json.getString("name");
+                                String email = json.getString("email");
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.putExtra("name", name);
+                                intent.putExtra("username", username);
+                                intent.putExtra("email", email);
+                                LoginActivity.this.startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage("Failed to login ! Make sure you have the good username and password")
+                                        .setNegativeButton("Rerty", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                LoginRequest loginRequest = new LoginRequest(username, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
             }
         });
     }
--- app/src/main/java/com/dumbaz/skatespot/MainActivity.java
+++ app/src/main/java/com/dumbaz/skatespot/MainActivity.java
@@ -1,5 +1,6 @@
 package com.dumbaz.skatespot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -12,8 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
-        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
+        final TextView etUsername = (TextView) findViewById(R.id.etUsername);
+        final TextView etEmail = (TextView) findViewById(R.id.etEmail);
         final TextView welcomeMessage = (TextView) findViewById(R.id.WelcomeMessage);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String username = intent.getStringExtra("username");
+        String email = intent.getStringExtra("email");
+
+        String message = "Welcome to the user area " + name;
+        welcomeMessage.setText(message);
+        etUsername.setText(username);
+        etEmail.setText(email);
     }
 }
