80e959ec119f023ca266a17c1b9f533eecbdec34
GwendalBazin <gwendal.bazin@viaccess-orca.fr>
2017-08-03 16:47:54 +0200

    add pin on map

---
+++ app/src/main/java/com/dumbaz/skatespot/GPSTracker.java
@@ -0,0 +1,61 @@
+package com.dumbaz.skatespot;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+
+/**
+ * Created by BAZIN on 8/3/2017.
+ */
+
+public class GPSTracker implements LocationListener {
+    Context context;
+    public GPSTracker(Context c) {
+        super();
+        this.context = c;
+    }
+
+    public Location getLocation() {
+
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(context, "Permission not granted", Toast.LENGTH_SHORT).show();
+            return null;
+        }
+        LocationManager lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        boolean isGPSEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (isGPSEnabled) {
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 10, this);
+            Location l = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            return l;
+        } else {
+            Toast.makeText(context, "Please enable GPS !", Toast.LENGTH_LONG).show();
+        }
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
--- app/src/main/java/com/dumbaz/skatespot/MapsActivity.java
+++ app/src/main/java/com/dumbaz/skatespot/MapsActivity.java
@@ -1,7 +1,12 @@
 package com.dumbaz.skatespot;
 
+import android.Manifest;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,10 +14,15 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    GPSTracker g;
+    Location l;
+    private LatLng me;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +32,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        final Button bAddMarker = (Button) findViewById(R.id.addSpotButton);
+        ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+        bAddMarker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addMarker();
+            }
+        });
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(new LatLng(48.1119800,  -1.6742900), 14, 45, 0)));
+        g = new GPSTracker(getApplicationContext());
+        l = g.getLocation();
+        me = new LatLng(l.getLatitude(), l.getLongitude());
+    }
+
+    public void addMarker() {
+        mMap.addMarker(new MarkerOptions().position(me).draggable(true));
     }
 }
